5e8d452aa86b4b79b1fabc8f576cc37ebae965db
geta-hachiware <toorucookies@gmail.com>
2025-07-24 21:06:54 +0800

    new changes

--- home-manager/modules/hyprland/default.nix
+++ home-manager/modules/hyprland/default.nix
@@ -22,8 +22,23 @@ in
 
       #Env
       env = [
-        "XCURSOR_SIZE,15"
-        "HYPRCURSOR_SIZE,15"
+        "XDG_CURRENT_DESKTOP,Hyprland"
+        "XDG_SESSION_DESKTOP,Hyprland"
+        "XDG_SESSION_TYPE,wayland"
+        "GDK_BACKEND,wayland,x11,*"
+        "NIXOS_OZONE_WL,1"
+        "ELECTRON_OZONE_PLATFORM_HINT,auto"
+        "MOZ_ENABLE_WAYLAND,1"
+        "OZONE_PLATFORM,wayland"
+        "EGL_PLATFORM,wayland"
+        "CLUTTER_BACKEND,wayland"
+        "SDL_VIDEODRIVER,wayland"
+        "QT_QPA_PLATFORM,wayland;xcb"
+        "QT_WAYLAND_DISABLE_WINDOWDECORATION,1"
+        "QT_QPA_PLATFORMTHEME,qt6ct"
+        "QT_AUTO_SCREEN_SCALE_FACTOR,1"
+        "WLR_RENDERER_ALLOW_SOFTWARE,1"
+        "NIXPKGS_ALLOW_UNFREE,1"
       ];
 
       #Look and Feel
