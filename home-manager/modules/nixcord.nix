84a6c81cb704a7b32d9e4d0790ec5dcc60ca022b
hachi <toorucookies@gmail.com>
2025-07-26 00:11:46 +0800

    added nixcord

---
+++ home-manager/modules/nixcord.nix
@@ -0,0 +1,40 @@
+{ pkgs, inputs, ... }:
+let
+  # system = "x86_64-linux";
+  home.packages = [ inputs.nixcord.${pkgs.system}.default ];
+in {
+  programs.nixcord = {
+    enable = true;
+    # vesktop = { enable = true; };
+    discord = {
+      enable = true;
+      package = pkgs.discord;
+      vencord = {
+        enable = true;
+        unstable = true;
+      };
+    };
+    # quickCss = "some CSS";  # quickCSS file
+    config = {
+      useQuickCss = true; # use out quickCSS
+      themeLinks = [ # or use an online theme
+        "https://refact0r.github.io/system24/theme/system24.theme.css"
+      ];
+
+      # frameless = true; # set some Vencord options
+      # plugins = {
+      #   hideAttachments.enable = true;    # Enable a Vencord plugin
+      #   ignoreActivities = {    # Enable a plugin and set some options
+      #     enable = true;
+      #     ignorePlaying = true;
+      #     ignoreWatching = true;
+      #     ignoredActivities = [ "someActivity" ];
+      #   };
+      # };
+    };
+    extraConfig = {
+      # Some extra JSON config here
+      # ...
+    };
+  };
+}
