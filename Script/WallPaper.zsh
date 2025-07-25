9bf570c071a4bf67bcd9862ff8e74a22048381d1
hachi <toorucookies@gmail.com>
2025-07-25 20:27:27 +0800

    Remove deleted folders/files

--- Script/WallPaper.zsh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-wallpapers=(~/hachi/Wallpaper/*.{jpg,jpeg,png,gif})
-wall="${wallpapers[RANDOM % ${#wallpapers[@]}]}"
-pgrep -x swww-daemon >/dev/null || swww-daemon &
-sleep 1
-swww img "$wall"
-magick "$wall" ~/.config/hypr/wallpaper.jpg
