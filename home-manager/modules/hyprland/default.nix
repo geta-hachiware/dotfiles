27319e5ebd4aaa97195bbd8f37c00ab13513f039
hachi <toorucookies@gmail.com>
2025-07-26 18:13:11 +0800

    fixed nixcord

--- home-manager/modules/hyprland/default.nix
+++ home-manager/modules/hyprland/default.nix
@@ -101,6 +101,7 @@
           "nofocus,class:^$,title:^$,xwayland:1,floating:1,fullscreen:0,pinned:0"
         ];
         windowrulev2 = [
+	  "opacity 1.0 override, class:^.*, fullscreen:1"
           "idleinhibit fullscreen, class:.*"
         ];
 
